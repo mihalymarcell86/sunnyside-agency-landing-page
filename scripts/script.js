d32e8a726fca21c20175418bc9a2565c04510cb8
Marcell Mihály <mihalymarcell86@gmail.com>
2022-08-06 23:38:46 +0100

    mobile menu animation

--- scripts/script.js
+++ scripts/script.js
@@ -3,21 +3,45 @@ const navigation = document.querySelector(".header__navigation");
 
 let mobileMenuOpen = false;
 
-function toggleMobileMenu() {
-  if (!mobileMenuOpen) {
-    navigation.style.display = "flex";
-    mobileMenuOpen = true;
-  } else closeMobileMenu();
+function openMobileMenu() {
+  navigation.style.display = "flex";
+  mobileMenuOpen = true;
+  gsap.fromTo(
+    navigation,
+    {
+      transform: "scale(0.7)",
+      opacity: 0,
+    },
+    { opacity: 1, ease: "back.out(4)", duration: 0.3, transform: "scale(1)" }
+  );
 }
 
 function closeMobileMenu() {
-  navigation.style.display = "none";
-  mobileMenuOpen = false;
+  gsap.to(navigation, { duration: 0.3, opacity: 0 }).then(() => {
+    navigation.style.display = "none";
+    mobileMenuOpen = false;
+  });
+}
+
+function toggleMobileMenu() {
+  if (!mobileMenuOpen) openMobileMenu();
+  else closeMobileMenu();
 }
 
 document.addEventListener("click", (event) => {
   if (event.target === hamburger) toggleMobileMenu();
-  else if (!event.target.closest(".header__navigation")) {
+  else if (
+    !event.target.closest(".header__navigation") &&
+    window.innerWidth < 1000
+  ) {
     closeMobileMenu();
   }
+});
+
+window.addEventListener("resize", () => {
+  if (window.innerWidth >= 1000) {
+    navigation.style.display = "flex";
+    navigation.style.opacity = "1";
+  } else navigation.style.display = "none";
+  mobileMenuOpen = false;
 });
